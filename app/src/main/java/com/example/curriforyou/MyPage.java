e5d5eab7d0140fa9bc998966aa861ae3aa144178
godchanny <godchanny@naver.com>
2022-09-24 17:48:43 +0900

    shinchaeeun some image changes add Mypage bottomNavi

--- app/src/main/java/com/example/curriforyou/MyPage.java
+++ app/src/main/java/com/example/curriforyou/MyPage.java
@@ -1,18 +1,17 @@
 package com.example.curriforyou;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MyPage extends AppCompatActivity {
+public class MyPage extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +28,7 @@ public class MyPage extends AppCompatActivity {
 
         TextView user_nickname = (TextView) findViewById(R.id.user_nickname);
         TextView user_position = (TextView) findViewById(R.id.user_position);
-        Button btn_addMajor = (Button) findViewById(R.id.btn_addMajor);
+        ImageView btn_addMajor = (ImageView) findViewById(R.id.btn_addMajor);
         Button btn_doubleMajor = (Button) findViewById(R.id.btn_doubleMajor);
         Button btn_minor = (Button) findViewById(R.id.btn_minor);
 
@@ -37,10 +36,48 @@ public class MyPage extends AppCompatActivity {
         btn_infoModification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent_myPage_infoModification = new Intent(getApplicationContext(), MyPage_infoModification.class);
-                startActivity(intent_myPage_infoModification); //액티비티 이동
+                Intent intent = new Intent(getApplicationContext(), MyPage_infoModification.class);
+                startActivity(intent); //액티비티 이동
             }
         });
 
+        LinearLayout naviBtn_curriculum = (LinearLayout) findViewById(R.id.naviBtn_curriculum);
+        LinearLayout naviBtn_jjimList = (LinearLayout) findViewById(R.id.naviBtn_jjimList);
+        LinearLayout naviBtn_lectureRecommendation = (LinearLayout) findViewById(R.id.naviBtn_lectureRecommendation);
+        LinearLayout naviBtn_gradeManagement = (LinearLayout) findViewById(R.id.naviBtn_gradeManagement);
+        LinearLayout naviBtn_myPage = (LinearLayout) findViewById(R.id.naviBtn_myPage);
+
+        naviBtn_curriculum.setOnClickListener(this);
+        naviBtn_jjimList.setOnClickListener(this);
+        naviBtn_lectureRecommendation.setOnClickListener(this);
+        naviBtn_gradeManagement.setOnClickListener(this);
+        naviBtn_myPage.setOnClickListener(this);
+
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.naviBtn_curriculum:
+                Intent intent_curriculum = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent_curriculum);
+                break;
+            /*case R.id.naviBtn_jjimList:
+                Intent intent_jjimList = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent_jjimList);
+                break;
+            case R.id.naviBtn_lectureRecommendation:
+                Intent intent_lectureRecommendation = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent_lectureRecommendation);
+                break;
+            case R.id.naviBtn_gradeManagement:
+                Intent intent_gradeManagement = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent_gradeManagement);
+                break;*/
+            case R.id.naviBtn_myPage:
+                Intent intent_myPage = new Intent(getApplicationContext(), MyPage.class);
+                startActivity(intent_myPage);
+                break;
+
+        }
     }
 }
--- app/src/main/java/com/example/curriforyou/MyPage_infoModification.java
+++ app/src/main/java/com/example/curriforyou/MyPage_infoModification.java
@@ -7,14 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MyPage_infoModification extends AppCompatActivity {
 
-    private TextView tv_infoModification;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage_info_modification);
-
-        tv_infoModification = findViewById(R.id.tv_infoModification);
-
+        TextView tv_infoModification = (TextView) findViewById(R.id.tv_infoModification);
     }
 }
