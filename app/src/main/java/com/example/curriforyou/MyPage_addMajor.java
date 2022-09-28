e069314be4793b143f3f79d4f78875bd4ec86e2e
godchanny <godchanny@naver.com>
2022-09-28 16:04:29 +0900

    Shinchaeeun add mypage_add_major

---
+++ app/src/main/java/com/example/curriforyou/MyPage_addMajor.java
@@ -0,0 +1,56 @@
+package com.example.curriforyou;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyPage_addMajor extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage_add_major);
+        LinearLayout naviBtn_curriculum = (LinearLayout) findViewById(R.id.naviBtn_curriculum);
+        LinearLayout naviBtn_jjimList = (LinearLayout) findViewById(R.id.naviBtn_jjimList);
+        LinearLayout naviBtn_lectureRecommendation = (LinearLayout) findViewById(R.id.naviBtn_lectureRecommendation);
+        LinearLayout naviBtn_gradeManagement = (LinearLayout) findViewById(R.id.naviBtn_gradeManagement);
+        LinearLayout naviBtn_myPage = (LinearLayout) findViewById(R.id.naviBtn_myPage);
+
+        naviBtn_curriculum.setOnClickListener(this);
+        naviBtn_jjimList.setOnClickListener(this);
+        naviBtn_lectureRecommendation.setOnClickListener(this);
+        naviBtn_gradeManagement.setOnClickListener(this);
+        naviBtn_myPage.setOnClickListener(this);
+
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.naviBtn_curriculum:
+                Intent intent_curriculum = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent_curriculum);
+                break;
+            /*case R.id.naviBtn_jjimList:
+                Intent intent_jjimList = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent_jjimList);
+                break;*/
+            case R.id.naviBtn_lectureRecommendation:
+                Intent intent_lectureRecommendation = new Intent(getApplicationContext(), RecomActivity.class);
+                startActivity(intent_lectureRecommendation);
+                break;
+            case R.id.naviBtn_gradeManagement:
+                Intent intent_gradeManagement = new Intent(getApplicationContext(), GmActivity.class);
+                startActivity(intent_gradeManagement);
+                break;
+            case R.id.naviBtn_myPage:
+                Intent intent_myPage = new Intent(getApplicationContext(), MyPageActivity.class);
+                startActivity(intent_myPage);
+                break;
+        }
+
+    }
+
+}
