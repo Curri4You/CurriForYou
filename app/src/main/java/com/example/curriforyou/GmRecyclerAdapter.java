12bc33ba7757746e5f0f7b2ff994186757c3a95e
godchanny <godchanny@naver.com>
2022-10-16 14:54:52 +0900

    Shinchaeeun gm_semester 학기별 학점 DB 연결

---
+++ app/src/main/java/com/example/curriforyou/GmRecyclerAdapter.java
@@ -0,0 +1,135 @@
+// 학점관리 학기별 요약을 위한 RecyclerAdapter & ViewHolder
+
+package com.example.curriforyou;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class GmRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    // adapter에 들어갈 list
+    private ArrayList<DataGmSemester> listData = new ArrayList<>();
+    private Context context;
+    // Item의 클릭 상태를 저장할 array 객체
+    private SparseBooleanArray selectedItems = new SparseBooleanArray();
+    // 직전에 클릭됐던 Item의 position
+    private int prePosition = -1;
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.listview_semester, parent, false);
+        return new GmViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ((GmViewHolder) holder).onBind(listData.get(position), position);
+    }
+
+    @Override
+    public int getItemCount() {
+        //RecyclerView의 총 개수
+        return listData.size();
+    }
+
+    void addItem(DataGmSemester data) {
+        // 외부에서 item을 추가시킬 함수
+        listData.add(data);
+    }
+
+    class GmViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView gm_list_semester, gm_list_totalGrade, gm_list_majorGrade, gm_list_liberalGrade;
+        private LinearLayout open_gm_semester;
+        private DataGmSemester data;
+        private int position;
+
+        public GmViewHolder(@NonNull View itemView) {
+            super(itemView);
+            gm_list_semester = itemView.findViewById(R.id.gm_list_semester);
+            gm_list_totalGrade = itemView.findViewById(R.id.gm_list_totalGrade);
+            gm_list_majorGrade = itemView.findViewById(R.id.gm_list_majorGrade);
+            gm_list_liberalGrade = itemView.findViewById(R.id.gm_list_liberalGrade);
+            open_gm_semester = itemView.findViewById(R.id.open_gm_semester);
+        }
+
+        //데이터 이용해서 text 변경
+        void onBind(DataGmSemester data, int position) {
+            this.data = data;
+            this.position = position;
+
+            gm_list_semester.setText(data.getSemester());
+            gm_list_totalGrade.setText(data.getTotalGrade());
+            gm_list_majorGrade.setText(data.getMajorGrade());
+            gm_list_liberalGrade.setText(data.getLiberalGrade());
+
+            changeVisibility(selectedItems.get(position));
+
+            open_gm_semester.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.open_gm_semester:
+                    if (selectedItems.get(position)) {
+                        // 펼쳐진 Item을 클릭 시
+                        selectedItems.delete(position);
+                    } else {
+                        // 직전의 클릭됐던 Item의 클릭상태를 지움
+                        selectedItems.delete(prePosition);
+                        // 클릭한 Item의 position을 저장
+                        selectedItems.put(position, true);
+                    }
+                    // 해당 포지션의 변화를 알림
+                    if (prePosition != -1)
+                        notifyItemChanged(prePosition);
+                    notifyItemChanged(position);
+                    // 클릭된 position 저장
+                    prePosition = position;
+                    break;
+            }
+        }
+
+        //         클릭된 Item의 상태 변경
+//         @param isExpanded Item을 펼칠 것인지 여부
+        private void changeVisibility(final boolean isExpanded) {
+            // height 값을 dp로 지정해서 넣고싶으면 아래 소스를 이용
+            int dpValue = 150;
+            float d = context.getResources().getDisplayMetrics().density;
+            int height = (int) (dpValue * d);
+
+            // ValueAnimator.ofInt(int... values)는 View가 변할 값을 지정, 인자는 int 배열
+            ValueAnimator va = isExpanded ? ValueAnimator.ofInt(0, height) : ValueAnimator.ofInt(height, 0);
+            // Animation이 실행되는 시간, n/1000초
+            va.setDuration(600);
+            va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    // value는 height 값
+                    int value = (int) animation.getAnimatedValue();
+                    // imageView의 높이 변경
+//                    imageView2.getLayoutParams().height = value;
+//                    imageView2.requestLayout();
+//                    // imageView가 실제로 사라지게하는 부분
+//                    imageView2.setVisibility(isExpanded ? View.VISIBLE : View.GONE);
+                }
+            });
+            // Animation start
+            va.start();
+        }
+    }
+}
--- app/src/main/java/com/example/curriforyou/GmSemesterActivity.java
+++ app/src/main/java/com/example/curriforyou/GmSemesterActivity.java
@@ -2,8 +2,12 @@
 
 package com.example.curriforyou;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -11,17 +15,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class GmSemesterActivity extends AppCompatActivity {
 
     //Adapter 연결
-    GmRecyclerVierAdapter adapter;
+    GmRecyclerAdapter adapter;
+    // [RecyclerView] 리스트 출력을 위한 parameter
+    private static final String TAG = "imagesearchexample";
+    // URL - 학기별 학점요약 DB
+    private  String REQUEST_URL = "http://smlee099.dothome.co.kr/practice_avg_grade.php";
+    public static final int LOAD_SUCCESS = 101;
+    private ProgressDialog progressDialog = null;
+    GmRecyclerAdapter rc_adapter = null;
+    private ArrayList<HashMap<String, String>> gmSemesterList = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gm_semester);
         init();
-        getData();
+        getData(REQUEST_URL);
+
+        //[RecyclerView] HashMap 사용
+        gmSemesterList = new ArrayList<HashMap<String, String>>();
+        //[RecyclerView] 각 필드값을 매핑, from -> to
+        String[] from = new String[]{"semester", "totalGrade", "majorGrade", "liberalGrade"};
+        int[] to = new int[]{R.id.gm_list_semester, R.id.gm_list_totalGrade, R.id.gm_list_majorGrade, R.id.gm_list_liberalGrade};
+
+        //[RecyclerView] 로딩이 걸릴 경우 로딩 Dialog 출력
+        progressDialog = new ProgressDialog(GmSemesterActivity.this);
+        progressDialog.setMessage("Please wait ...");
+        progressDialog.show();
 
         //뒤로가기 버튼
         ImageView btn_infoModification = (ImageView) findViewById(R.id.navigate_before);
@@ -34,25 +70,125 @@ public class GmSemesterActivity extends AppCompatActivity {
         });
     }
 
+    // [RecyclerView] 연결
     private void init(){
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        RecyclerView rc_gm_semester = findViewById(R.id.rc_gm_semester);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        adapter = new GmRecyclerVierAdapter();
-        recyclerView.setAdapter(adapter);
+        rc_gm_semester.setLayoutManager(linearLayoutManager);
+        rc_adapter = new GmRecyclerAdapter();
+        rc_gm_semester.setAdapter(rc_adapter);
+    }
+
+    //[RecyclerView]
+    private final RC_MyHandler RC_mHandler = new RC_MyHandler(this);
+    //[RecyclerView]
+    private static class RC_MyHandler extends Handler {
+        private final WeakReference<GmSemesterActivity> weakReference;
+
+        public RC_MyHandler(GmSemesterActivity gmSemesterActivity){
+            weakReference = new WeakReference<GmSemesterActivity>(gmSemesterActivity);
+        }
+
+        public void handleMessage(Message msg){
+            GmSemesterActivity gmSemesterActivity = weakReference.get();
+
+            if(gmSemesterActivity != null){
+                switch (msg.what){
+                    case LOAD_SUCCESS:
+                        gmSemesterActivity.progressDialog.dismiss();
+                        gmSemesterActivity.rc_adapter.notifyDataSetChanged();
+                        break;
+                }
+            }
+        }
     }
 
     //데이터 넣어주기
-    private void getData(){
-        //semester(학기), totalGrade(총 학점), majorGrade(전공학점), liberalGrade(교양학점)
-        DataGmSemester data = new DataGmSemester("1학년 1학기", "3.25", "3.15", "3.38");
-        adapter.addItem(data);
-        data = new DataGmSemester("1학년 2학기", "3.53", "3.77", "3.30");
-        adapter.addItem(data);
-        data = new DataGmSemester("2학년 1학기", "3.48", "3.48", "3.48");
-        adapter.addItem(data);
-        data = new DataGmSemester("2학년 2학기", "3.39", "3.28", "4.00");
-        adapter.addItem(data);
+    private void getData(String Request_url){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String result;
+
+                try{
+                    Log.d(TAG, Request_url);
+                    URL url = new URL(Request_url);
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                    httpURLConnection.setReadTimeout(3000);
+                    httpURLConnection.setConnectTimeout(3000);
+                    httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setDoInput(true);
+                    httpURLConnection.setRequestMethod("GET");
+                    httpURLConnection.setUseCaches(false);
+                    httpURLConnection.connect();
+
+                    int responseStatusCode = httpURLConnection.getResponseCode();
+
+                    InputStream inputStream;
+                    if (responseStatusCode == HttpURLConnection.HTTP_OK) {
+                        inputStream = httpURLConnection.getInputStream();
+                    } else {
+                        inputStream = httpURLConnection.getErrorStream();
+                    }
+
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+
+                    while ((line = bufferedReader.readLine()) != null) {
+                        sb.append(line);
+                    }
+
+                    bufferedReader.close();
+                    httpURLConnection.disconnect();
+
+                    result = sb.toString().trim();
+                } catch (Exception e){
+                    result = e.toString();
+                }
+                if (RC_jsonParser(result)){     // 파싱 기능 사용
+                    Message message = RC_mHandler.obtainMessage(LOAD_SUCCESS);
+                    RC_mHandler.sendMessage(message);
+                }
+            }
+        });
+        thread.start();
+    }
+
+    //[RecyclerView] jsonParser 함수
+    //웹사이트 화면을 파싱하는 함수
+    public boolean RC_jsonParser(String jsonString) {
+
+        if (jsonString == null) return false;
+
+        try {
+            JSONObject jsonObject = new JSONObject(jsonString);
+
+            //전체 행렬 중 DB 내용 부분을 jsonArray 형태로 저장
+            JSONArray gmSemester = jsonObject.getJSONArray("practice_avg_grade");
+
+            gmSemesterList.clear();
+
+            //gmSemester 길이만큼 반복해서 Mapping
+            for (int i = 0; i < gmSemester.length(); i++) {
+                JSONObject gmSemesterInfo = gmSemester.getJSONObject(i);
+
+                String semester = gmSemesterInfo.getString("semester");
+                String totalGrade = gmSemesterInfo.getString("totalGrade");
+                String majorGrade = gmSemesterInfo.getString("majorGrade");
+                String liberalGrade = gmSemesterInfo.getString("liberalGrade");
+
+                DataGmSemester data = new DataGmSemester(semester, totalGrade, majorGrade, liberalGrade);
+                rc_adapter.addItem(data);
+
+            }
+            return true;
+        } catch (JSONException e) {
+            Log.d(TAG, e.toString());
+        }
+        return false;
     }
 
 }
